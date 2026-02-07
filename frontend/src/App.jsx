ecafc10931bb5be22979745385b124077c11b374
Josh Woolbright <woolbright.josh.t@gmail.com>
2026-02-07 15:24:29 -0700

    fixing url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import axios from 'axios'
 import './App.css'
 
-const API_URL = 'badurl'
+const API_URL = ''
 
 function App() {
   const [formData, setFormData] = useState({
